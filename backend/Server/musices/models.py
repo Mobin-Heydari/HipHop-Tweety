acb1c03bbc0a4d5639070666ad026f8d740e3ad8
Mobin_Developer <special.me.force1@gmail.com>
2024-04-24 17:58:28 +0330

    Music model added to musices models.py file

--- backend/Server/musices/models.py
+++ backend/Server/musices/models.py
@@ -1,3 +1,70 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Music(models.Model):
+    
+    name = models.CharField(
+        verbose_name="نام",
+        max_length=200,
+        unique=True
+    )
+    
+    artist = models.CharField(
+        verbose_name="آرتیست",
+        max_length=200
+    )
+    
+    Composer = models.CharField(
+        verbose_name="آهنگساز",
+        max_length=200
+    )
+    
+    Songwriter = models.CharField(
+        verbose_name="ترانه سرا",
+        max_length=200
+    )
+    
+    Regulators = models.CharField(
+        verbose_name="تنظیم کننده",
+        max_length=200
+    )
+    
+    image = models.ImageField(
+        upload_to="Musices/files/images",
+        verbose_name="تصویر موزیک"
+    )
+    
+    banner = models.ImageField(
+        upload_to="Musices/files/banner",
+        verbose_name="بنر موزیک"
+    )
+    
+    music_file = models.FileField(
+        upload_to="Musices/files/mp3",
+        verbose_name="فایل موزیک"
+    )
+    
+    text = models.TextField(
+        verbose_name="متن"
+    )
+
+    release_date = models.DateField(
+        verbose_name="تاریخ اکران"
+    )
+
+    likes = models.IntegerField(
+        verbose_name="لایک ها",
+        default=0
+    )
+    
+    music_time = models.CharField(
+        verbose_name="زمان موزیک",
+        max_length=20,
+    )
+
+    class Meta:
+        ordering = ['release_date']
+        verbose_name = "کاربر"
+        verbose_name_plural = "کاربر ها"
+        
