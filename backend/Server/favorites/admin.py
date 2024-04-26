13752effd6aa980e90f34c992999e02ecee445fb
Mobin_Developer <special.me.force1@gmail.com>
2024-04-26 11:16:24 +0330

    User favorites and music and albome faver added to the project.

--- backend/Server/favorites/admin.py
+++ backend/Server/favorites/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+class UserMusicFaverInline(admin.TabularInline):
+    model = models.UserMusicFaver
+    raw_id_fields = ['music']
+
+
+class UserAlbomeFaverInline(admin.TabularInline):
+    model = models.UserAlbomeFaver
+    raw_id_fields = ['albome']
+
+@admin.register(models.UserFavorite)
+class UserFavoritesAdmin(admin.ModelAdmin):
+    inlines = [UserMusicFaverInline, UserAlbomeFaverInline]
+    list_display = ['user', 'count']
--- backend/Server/favorites/models.py
+++ backend/Server/favorites/models.py
@@ -1,3 +1,111 @@
 from django.db import models
+from alboms.models import Albom
+from musices.models import Music
+from users.models import User
 
-# Create your models here.
+
+
+
+class UserFavorite(models.Model):
+    
+    user = models.ForeignKey(
+        User,
+        verbose_name="کاربر",
+        on_delete=models.CASCADE,
+        related_name="user_favorites"
+    )
+    
+    slug = models.SlugField(
+        verbose_name="اسلاگ",
+        unique=True
+    )
+    
+    count = models.IntegerField(
+        default=0,
+        verbose_name="تعداد علاقه مندی ها"
+    )
+    
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ['user']
+        verbose_name = "علاقمندی های کاربر"
+        verbose_name_plural = "علاقه مندی های کاربران"
+        
+    
+    def __str__(self):
+        return f'علاقمندی های کاربر : {self.user.username}'
+    
+
+
+class UserAlbomeFaver(models.Model):
+    
+    user_favorite = models.ForeignKey(
+        UserFavorite,
+        on_delete=models.CASCADE,
+        verbose_name="علاقمندی های کاربر",
+        related_name="user_albome_faver"
+    )
+    
+    
+    albome = models.ForeignKey(
+        Albom,
+        verbose_name="آلبوم",
+        on_delete=models.CASCADE,
+        related_name="albome_faver"
+    )
+    
+    
+    is_faver = models.BooleanField(
+        default=False,
+        verbose_name="علاقه دارد؟"
+    )
+    
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    
+    class Meta:
+        ordering = ['is_faver', 'updated']
+        verbose_name = "علاقمندی آلبوم های کاربر"
+        verbose_name_plural = "علاقمندی آلبوم های کاربران"
+        
+    
+    def __str__(self):
+        return f'{self.albome.title}'
+
+
+class UserMusicFaver(models.Model):
+    user_favorite = models.ForeignKey(
+        UserFavorite,
+        on_delete=models.CASCADE,
+        verbose_name="علاقمندی های کاربر",
+        related_name="user_music_faver"
+    )
+    
+    music = models.ForeignKey(
+        Music,
+        verbose_name="آهنگ",
+        on_delete=models.CASCADE,
+        related_name="music_faver"
+    )
+    
+    
+    is_faver = models.BooleanField(
+        default=False,
+        verbose_name="علاقه دارد؟"
+    )
+    
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    
+    class Meta:
+        ordering = ['is_faver', 'updated']
+        verbose_name = "علاقمندی موزیک های کاربر"
+        verbose_name_plural = "علاقمندی موزیک های کاربران"
+        
+    
+    def __str__(self):
+        return f'{self.music.title}'
