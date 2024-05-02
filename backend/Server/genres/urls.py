e6323befc8538b97185f87472aa6e34efe9cdcf2
Mobin_Developer <special.me.force1@gmail.com>
2024-05-02 15:39:47 +0330

    Genre detail added to the project.

--- backend/Server/genres/urls.py
+++ backend/Server/genres/urls.py
@@ -8,4 +8,5 @@ app_name = "genres"
 urlpatterns = [
     # Genre Url
     path('', views.GenresView.as_view(), name="genres"),
+    path('detail/<slug:slug>', views.GenreDetail.as_view(), name="genre_detail"),
 ]
--- backend/Server/genres/views.py
+++ backend/Server/genres/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import View
 from .models import Genre
+from musices.models import Music
 
 
 class GenresView(View):
@@ -15,4 +16,22 @@ class GenresView(View):
             }
         )
 
+
+
+class GenreDetail(View):
+    
+    def get(self, request, slug):
         
+        genre = get_object_or_404(Genre, slug=slug)
+        
+        genere_musices = Music.objects.filter(genre=genre).order_by('-likes')
+        
+        genres = Genre.objects.all().order_by('?')
+        
+        return render(
+            request, 'genres/detail.html', {
+                'genre' : genre,
+                'genres' : genres,
+                'genere_musices' : genere_musices
+            }
+        )
