1d7ad1f66aa6dfeab33e349ea07418f90775a830
Mobin_Developer <special.me.force1@gmail.com>
2024-05-02 09:57:22 +0330

    Emailing settings added

--- backend/Server/Server/settings.py
+++ backend/Server/Server/settings.py
@@ -130,3 +130,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Auth user model
 AUTH_USER_MODEL = 'users.User'
+
+
+# Emailing Settings
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "special.me.force1@gmail.com"
+EMAIL_HOST_PASSWORD = "SZPM7008"
+EMAIL_USE_TLS = True
+# EMAIL_USE_SSL = False
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
