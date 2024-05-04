0597374a04dbee3f0946c388a441f49c3e5c7324
Mobin_Developer <special.me.force1@gmail.com>
2024-05-04 12:42:45 +0330

    Otp model added to the project

--- backend/Server/authentication/models.py
+++ backend/Server/authentication/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.core import validators
 
-# Create your models here.
+
+
+
+class Otp(models.Model):
+    
+    email = models.EmailField(
+        max_length=255,
+        verbose_name="ایمیل",
+        validators=[
+            validators.EmailValidator
+        ]
+    )
+    
+    username = models.CharField(
+        max_length=255,
+        verbose_name="نام کاربری"
+    )
+    
+    password = models.CharField(
+        max_length=255,
+        verbose_name="رمز عبور"
+    )
+    
+    otp_code = models.IntegerField(
+        verbose_name="کد otp"
+    )
+    
+    id = models.CharField(
+        verbose_name="آیدی", max_length=500,
+        primary_key=True, unique=True,
+
+    )
+    
+    class Meta:
+        verbose_name = "کد اعتبارسنجی"
+        verbose_name_plural = "کدهای اعتبارسنجی"
+        
+        
+    def __str__(self):
+        return f'{self.id}---{self.email}---{self.otp_code}'
