8ebcfc4423290c183bc8d8fd4979ebfd2a7f16cf
Mobin_Developer <special.me.force1@gmail.com>
2024-05-09 23:42:26 +0330

    Plans section in landingpage is optimized

--- backend/Server/landing/views.py
+++ backend/Server/landing/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import View
+from subscription.models import SubscriptionPlan
 
 
 
@@ -8,8 +9,10 @@ class LandingPageView(View):
     
     def get(self, request):
         
+        plans = SubscriptionPlan.objects.filter(is_active=True)
+        
         return render(
             request, 'landing/index.html', {
-                
+                'plans' : plans
             }
         )
