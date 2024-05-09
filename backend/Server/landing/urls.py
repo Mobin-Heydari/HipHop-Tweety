13680ee3cbccc002875b3164640431c77202ac68
Mobin_Developer <special.me.force1@gmail.com>
2024-05-10 00:14:33 +0330

    Contacts system is compeleted

--- backend/Server/landing/urls.py
+++ backend/Server/landing/urls.py
@@ -8,5 +8,6 @@ app_name = "landing"
 
 
 urlpatterns = [
-    path('', views.LandingPageView.as_view(), name="landing")
+    path('', views.LandingPageView.as_view(), name="landing"),
+    path('contact/', views.ContactView.as_view(), name="contact")
 ]
