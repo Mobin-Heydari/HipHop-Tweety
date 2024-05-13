be63acfa57623caeefbf624df1b5e5a076e916ce
Mobin_Developer <special.me.force1@gmail.com>
2024-05-13 14:06:33 +0330

    Asgi.py file is optimized for js res and req

--- backend/Server/Server/asgi.py
+++ backend/Server/Server/asgi.py
@@ -1,16 +1,11 @@
-"""
-ASGI config for Server project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
-"""
-
 import os
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.urls import path
+from myapp.consumers import TestConsumer
 
-from django.core.asgi import get_asgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Server.settings')
-
-application = get_asgi_application()
+ws_patterns = [
+    path('ws/test/', TestConsumer)
+]
+application = ProtocolTypeRouter({
+    'websocket': URLRouter(ws_patterns)
+})
