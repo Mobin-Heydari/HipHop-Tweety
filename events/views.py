0c1dc9e8c3238264f848c85c7dbeb216771f1722
Mobin_Developer <special.me.force1@gmail.com>
2024-05-20 16:54:00 +0330

    Events list and detail views added to the project.

--- events/views.py
+++ events/views.py
@@ -1,3 +1,68 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views import View
+from django.core.paginator import Paginator
+from subscription.models import UserSubscription
+from .models import Event, EventDescription
 
-# Create your views here.
+
+
+class EventsList(View):
+    
+    def get(self, request):
+        
+        if request.user.is_authenticated == True:
+            
+            try:
+                user_sub = UserSubscription.objects.get(user=request.user)
+                
+                user_sub.validate_subscription()
+                
+                if user_sub.is_active == True:
+                    
+                    queryset = Event.objects.all().order_by('-event_date')
+                    
+                    page_number = request.GET.get('page')
+                    paginator = Paginator(queryset, 8)
+                    queryset = paginator.get_page(page_number)
+                    
+                    return render(
+                        request, 'events/events_list.html', {
+                            'event': queryset,
+                        }
+                    )
+                else:
+                    return redirect('subscription:plans')
+            except:
+                return redirect('subscription:plans')
+        else:
+            return redirect('authentication:login')
+        
+
+class EventDetail(View):
+    
+    def get(self, request, slug):
+        
+        if request.user.is_authenticated == True:
+            
+            try:
+                user_sub = UserSubscription.objects.get(user=request.user)
+                
+                user_sub.validate_subscription()
+                
+                if user_sub.is_active == True:
+                    
+                    event = get_object_or_404(
+                        Event, slug=slug
+                    )
+                    
+                    return render(
+                        request, 'events/events_list.html', {
+                            'event': event,
+                        }
+                    )
+                else:
+                    return redirect('subscription:plans')
+            except:
+                return redirect('subscription:plans')
+        else:
+            return redirect('authentication:login')
