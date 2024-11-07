23ba6d9a1d91ed8a6958140051c7af120213179b
Mobin_Developer <special.me.force1@gmail.com>
2024-11-07 08:23:34 +0330

    project moddified.

--- search/views.py
+++ search/views.py
@@ -13,41 +13,38 @@ class SearchView(View):
         
         if request.user.is_authenticated == True:
             
-            try:
-                user_sub = UserSubscription.objects.get(user=request.user)
+            user_sub = UserSubscription.objects.get(user=request.user)
                 
-                user_sub.validate_subscription()
+            user_sub.validate_subscription()
+            
+            if user_sub.is_active == True:
                 
-                if user_sub.is_active == True:
+                searched_data = request.GET.get('q')
+    
+                if searched_data is not None:
+    
+                    searched_musices = Music.objects.filter(title__icontains=searched_data)
                     
-                    searched_data = request.GET.get('q')
-        
-                    if searched_data is not None:
-        
-                        searched_musices = Music.objects.filter(title__icontains=searched_data)
+                    searched_albumes = Albom.objects.filter(title__icontains=searched_data)
+                    
+                    if searched_albumes and searched_musices is not None:
                         
-                        searched_albumes = Albom.objects.filter(title__icontains=searched_data)
+                        page_number = request.GET.get('page')
+                        paginator = Paginator(searched_musices, 8)
+                        queryset = paginator.get_page(page_number)
                         
-                        if searched_albumes and searched_musices is not None:
-                            
-                            page_number = request.GET.get('page')
-                            paginator = Paginator(searched_musices, 8)
-                            queryset = paginator.get_page(page_number)
-                            
-                            return render(
-                                request, 'search/search.html', {
-                                    'searched_musices': queryset,
-                                    'searched_albumes' : searched_albumes,
-                                    'searched_data' : searched_data
-                                }
-                            )    
-                        else:
-                            raise Http404()
+                        return render(
+                            request, 'search/search.html', {
+                                'searched_musices': queryset,
+                                'searched_albumes' : searched_albumes,
+                                'searched_data' : searched_data
+                            }
+                        )    
                     else:
                         raise Http404()
                 else:
-                    return redirect('subscription:plans')
-            except:
+                    raise Http404()
+            else:
                 return redirect('subscription:plans')
         else:
             return redirect('authentication:login')
